4b7ecd42b6fad930f8d8a75d24ef96d72c36f442
Varun <168varun@gmail.com>
2025-06-10 12:00:00 +0530

    push

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Stop running containers and remove volumes
+echo "Stopping running containers..."
+docker-compose down -f
+# Pull latest images
+echo "Pulling latest images..."
+docker-compose pull
+# Start services
+echo "Starting services..."
+docker-compose up -d
+
+echo "Services started successfully!"
